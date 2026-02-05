2e84a2c393374cebf805739fc5c9ebe1dc904273
Huniity <adrien.dejonc@gmail.com>
2026-02-05 21:49:45 +0000

    FEAT: Added routes to vite project

--- aula34/aula-vite-project/src/App.tsx
+++ aula34/aula-vite-project/src/App.tsx
@@ -1,40 +1,22 @@
 
 import './App.css'
-import Button from './components/Button'
-import Card from './components/Card'
-import Counter from './components/Counter'
-
-const styles = {
-  width: "100vw", 
-  height: "100vh", 
-  display: "flex", 
-  alignItems: "center", 
-  justifyContent: "center",
-  textAlign: "center" as const,
-}
-
+import { Routes, Route } from 'react-router-dom'
+import { Home, About , Blog, Post, Search, NotFound } from './pages/Pages'
+import Navbar from './components/layout/Navbar'
 
 
 function App() {
   return (
-    <div style={ styles }>
-      <Card 
-        title="Exercise: Make a Card Component"
-        description="Welcome to the Frontend2 & SEO course where we are going to learn about React" 
-        profilePic="https://t4.ftcdn.net/jpg/02/72/31/41/360_F_272314124_oVQMMZWHwOPYsmCASwjRcFuZhSa4jqzO.jpg"
-        >
-        <Button
-          name="Click Me!"
-        />
-      </Card>
-      <Card 
-        title="Exercise: Make a Counter Component"
-        description="Interactive counter component"
-        altDescription="Tic-Tac!"
-        profilePic="https://t4.ftcdn.net/jpg/02/72/31/41/360_F_272314124_oVQMMZWHwOPYsmCASwjRcFuZhSa4jqzO.jpg"
-      >
-        <Counter/>
-      </Card>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+        <Route path='/blog/:id' element={<Post/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
